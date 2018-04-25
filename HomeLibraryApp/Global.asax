577eecaac68aee30fb7189b1c1154a7e33d8318d
wblachowski <wojciech@blachowski.pl>
2018-04-25 16:50:39 +0200

    Add project files.

---
+++ HomeLibraryApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeLibraryApp.MvcApplication" Language="C#" %>
---
+++ HomeLibraryApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeLibraryApp.Startup))]
+namespace HomeLibraryApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
