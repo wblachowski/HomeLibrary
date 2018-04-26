8fdb5822af3ff8a244a40339449aa6479327cd1c
wblachowski <wojciech@blachowski.pl>
2018-04-26 16:01:39 +0200

    Add materialize css

--- HomeLibraryApp/App_Start/BundleConfig.cs
+++ HomeLibraryApp/App_Start/BundleConfig.cs
@@ -24,8 +24,10 @@ namespace HomeLibraryApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/materialize/css/materialize.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
