169af3f2e798b9ee55ace433555a3ac6056f54e6
wblachowski <wojciech@blachowski.pl>
2018-04-26 16:36:56 +0200

    Fixed signing in

--- HomeLibraryApp/Controllers/AccountController.cs
+++ HomeLibraryApp/Controllers/AccountController.cs
@@ -74,7 +74,7 @@ namespace HomeLibraryApp.Controllers
             }
 
             // Require the user to have a confirmed email before they can log on.
-            var user = await UserManager.FindByNameAsync(model.Email);
+            var user = await UserManager.FindByEmailAsync(model.Email);
             if (user != null)
             {
                 if (!await UserManager.IsEmailConfirmedAsync(user.Id))
@@ -88,7 +88,7 @@ namespace HomeLibraryApp.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -164,7 +164,7 @@ namespace HomeLibraryApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
