b41b6d92f9c60b70d75d4d4a44a5cf2ccdd9c67e
wblachowski <wojciech@blachowski.pl>
2018-04-27 15:32:36 +0200

    Change inputfields to animated ones

--- HomeLibraryApp/App_Start/BundleConfig.cs
+++ HomeLibraryApp/App_Start/BundleConfig.cs
@@ -14,11 +14,16 @@ namespace HomeLibraryApp
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                        "~/Scripts/materialize/materialize.js"));
+
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+   
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
