7f4b122f2ed105b0ae8a06d17e78bab301240381
wblachowski <wojciech@blachowski.pl>
2018-05-02 12:31:26 +0200

    Add a new book modal

--- HomeLibraryApp/App_Start/BundleConfig.cs
+++ HomeLibraryApp/App_Start/BundleConfig.cs
@@ -27,14 +27,8 @@ namespace HomeLibraryApp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/materialize/css/materialize.css",
-                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"
                       ));
         }
--- HomeLibraryApp/Scripts/site.js
+++ HomeLibraryApp/Scripts/site.js
@@ -3,10 +3,13 @@
     $('.tabs').tabs();
     $('.modal').modal({ onOpenEnd: function () { $('.tabs').tabs();}});
     $('.sidenav').sidenav();
+    $('.datepicker').datepicker({
+        firstDay: 1
+    });
 });
 
 function onInvitationSuccess(result) {
     msg = result ? "The invitation has been sent" : "There was a problem while sending the invitation";
-    console.log(msg);
+    console.log(msg);s
     M.toast({ html: msg });
 }
