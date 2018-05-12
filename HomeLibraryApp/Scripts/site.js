8f10387b57f15c8ddd8eceafb6bee265fb853111
wblachowski <wojciech@blachowski.pl>
2018-05-12 12:51:05 +0200

    Goodreads base

--- HomeLibraryApp/Scripts/site.js
+++ HomeLibraryApp/Scripts/site.js
@@ -37,3 +37,55 @@ function searchBookAddClick(id) {
     $('#search-book-confirm-modal').modal();
     $('#search-book-confirm-modal').modal('open');
 }
+
+function scanGoodreads() {
+    $("#goodreads-error").hide();
+    link = $("#goodreads-input").val();
+    if (link.trim() == "") {
+        $('#goodreads .preloader-wrapper').hide();
+        $("#goodreads-result").hide();
+        return;
+    }
+
+    $('#goodreads .preloader-wrapper').show();
+    $("#goodreads-result").hide();
+    $.ajax({
+        url: "https://cors.io/?"+link,
+        success: function (result) {
+            html = $.parseHTML(result);
+            author = $(html).find("span[itemprop='name']").html().split(" ");
+            title = $(html).find("#bookTitle").text().trim();
+            publicationData = $(html).find("div:contains('Published'):not(:has(div))").text();
+            publisher = publicationData.substring(publicationData.lastIndexOf("by ") + 3, publicationData.length);
+            publicationDate = publicationData.substring(publicationData.lastIndexOf("Published") + 10, publicationData.lastIndexOf("by ")).trim();
+            publicationDate = publicationDate.split(" ");
+            if (publicationDate.length == 1) {
+                month = "Jan"
+                day = "01"
+                year = publicationDate[0]
+            } else {
+                month = publicationDate[0].substring(0, 3);
+                day = publicationDate[1].replace(/[^0-9]/g, '');
+                if (day.length == 1) day = '0' + day;
+                year = publicationDate[2];
+            }
+
+
+            $('#goodreads .preloader-wrapper').hide();
+            $("#goodreads-result").show();
+
+            $("#goodreadsTitle").val(title);
+            $("#goodreadsFirstname").val(author[0]);
+            $("#goodreadsLastname").val(author[1]);
+            $("#goodreadsPublisher").val(publisher);
+            $("#goodreadsDate").val(month + " " + day + ", " + year);
+
+            M.updateTextFields();
+        },
+        error: function (result) {
+            $('#goodreads .preloader-wrapper').hide();
+            $("#goodreads-result").hide();
+            $("#goodreads-error").show();
+        }
+    });
+}
